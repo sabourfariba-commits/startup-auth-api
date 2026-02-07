3af8f2e30f0b82ec090fd7a093f3931a74359865
sabourfariba-commits <sabourfariba@gmail.com>
2026-02-07 13:06:57 +0330

    add auth routes

--- src/src/routes/auth.routes.js
+++ src/src/routes/auth.routes.js
@@ -1,1 +1,12 @@
-add auth routes
+const express = require('express');
+const {
+  register,
+  login,
+} = require('../controllers/auth.controller');
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+
+module.exports = router;
