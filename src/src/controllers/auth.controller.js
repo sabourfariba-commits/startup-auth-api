2cde83549b56fe79cce35f1bd307d6b8f4fc981b
sabourfariba-commits <sabourfariba@gmail.com>
2026-02-07 13:08:03 +0330

    add mock auth controllers

--- src/src/controllers/auth.controller.js
+++ src/src/controllers/auth.controller.js
@@ -1,1 +1,15 @@
-add auth controller
+exports.register = (req, res) => {
+  const { email } = req.body;
+
+  res.status(201).json({
+    message: 'User registered successfully (mock)',
+    user: { email },
+  });
+};
+
+exports.login = (req, res) => {
+  res.status(200).json({
+    message: 'User logged in successfully (mock)',
+    token: 'fake-jwt-token',
+  });
+};
